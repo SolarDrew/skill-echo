3144be92d27a995dfa7f8e4b2d3ff509481e77cc
Drew Leonard <andy.j.leonard@gmail.com>
2021-01-31 14:19:48 +0000

    Well it's clunky and horrible and I don't like it but it mostly works

--- __init__.py
+++ __init__.py
@@ -6,6 +6,10 @@ from opsdroid.events import Message
 async def echo(opsdroid, config, message):
     conn = opsdroid.get_connector("matrix")
     nick, mxid = conn.nick, conn.mxid
-    if (nick in message.text) or (mxid in message.text):
-        newtext = message.text.split(":")[1].strip()
+    nick = nick if nick else "no nick, don't match this"
+    if nick in message.text:
+        newtext = message.text.split(nick)[1].strip("</a>").strip(":").strip()
+        await message.respond(newtext)
+    elif mxid in message.text:
+        newtext = message.text.split(mxid)[1].strip("</a>").strip(":").strip()
         await message.respond(newtext)
